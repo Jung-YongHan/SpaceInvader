c2bcccbe7aefdca1db0a4ac450a41f918b767747
Minjae Baek <qoralswo29@gmail.com>
2023-04-14 23:38:28 +0900

    score

--- src/main/java/org/newdawn/spaceinvaders/Game.java
+++ src/main/java/org/newdawn/spaceinvaders/Game.java
@@ -228,6 +228,7 @@ public class Game extends Canvas
 	public void notifyDeath() {
 		message = "Level "+level+", Score :"+ alienkill	;
 		waitingForKeyPress = true;
+		updateHighScore();
 		alienkill=0;
 
 	   //Rank.setScore((alienkill/(timer/1000)));
@@ -240,8 +241,16 @@ public class Game extends Canvas
 	public void notifyWin() {
 		message = "Level "+level+", Score :"+alienkill;
 		waitingForKeyPress = true;
+		updateHighScore();
 		alienkill=0;
 
+	}
+
+	private int highScore = 0;
+	public void updateHighScore() {
+		if (alienkill > highScore) {
+			highScore = alienkill;
+		}
 	}
 
 
@@ -434,7 +443,7 @@ public class Game extends Canvas
 			if (waitingForKeyPress) {
 				g.setColor(Color.white);
 				g.drawString(message,(800-g.getFontMetrics().stringWidth(message))/2,250);
-				g.drawString("Press any key",(800-g.getFontMetrics().stringWidth("Press any key"))/2,300);
+				g.drawString("Rank 1st's score : "+highScore+"  Press any key",(700-g.getFontMetrics().stringWidth("Press any key"))/2,300);
 				//타이머(스코어) 0 초기화
 				timer =0;
 			}
